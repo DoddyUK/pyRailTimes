68027d3a113aba80f00b6c3139258c58a045ef2d
Michael Dodd <doddyuk@gmail.com>
2021-03-20 17:22:41 +0000

    board.py looking a lot tidier

--- display/board.py
+++ display/board.py
@@ -3,79 +3,179 @@ import network.rttapi as api
 import parser.parser
 from config.config import Config
 
-class Ticker:
+class _Renderer:
+    def __init__(self, config):
+        self.__config = config
+
+    def message(self, message):
+        print("│ {:^{width}} │".format(message, width=self.__config.board_width - 4))
+
+    def left_message(self, message):
+        print("│ {:{width}} │".format(message, width=self.__config.board_width - 4))
+
+    def board_row(self, order, time, destination, expected, ):
+        print(
+            "│ {:<3}{:4>}  {:{width}} {:>8} │".format(
+                order,
+                time,
+                destination,
+                expected,
+                width=(self.__config.board_width - 22)
+            )
+        )
+
+    def __top(self):
+        print("┌{:─<{width}}┐".format('─', width=self.__config.board_width - 2))
+
+    def __divider(self):
+        print("├{:─<{width}}┤".format('─', width=self.__config.board_width - 2))
+
+    def __station_row(self, station, platform):
+        print(
+            "│ {name:{width}} │ {platform:>12} │".format(
+                name=station.name,
+                platform="Platform {}".format(platform),
+                width=(self.__config.board_width - 19)
+            )
+        )
+
+    def header(self, station, platform):
+        self.__top()
+        self.__station_row(station, platform)
+        self.__divider()
+        self.board_row("", "Time", "Destination", "Expected")
+
+    def service_row(self, index, service):
+        expected = "On Time" if (service.expectedTime == service.departureTime) else service.expectedTime
+        self.board_row(index, service.departureTime, service.destination, expected)
+
+    def blank_row(self):
+        self.message(" ")
+
+    def bottom_row(self):
+        print(
+            "└─{:─<{leftwidth}}{:^12}{:─<{rightwidth}}─┘".format(
+                "─",
+                datetime.datetime.now().strftime("%H:%M:%S"),
+                "─",
+                leftwidth=int((self.__config.board_width - 16) / 2) + (self.__config.board_width % 2),
+                rightwidth=int((self.__config.board_width - 16) / 2)
+            )
+        )
+
+
+class _Ticker:
     __message = ""
-    counter = 0
-    config = Config()
+    __counter = 0
+
+    def __init__(self, config, renderer):
+        self.__config = config
+        self.__renderer = renderer
 
     def set_message(self, message):
-        self.__message = "{:>{width}}".format(message, width=(self.config.board_width + len(message)))
-        self.counter = 0
+        self.__message = "{:>{width}}".format(message, width=(self.__config.board_width + len(message)))
+        self.__counter = 0
 
     def render(self):
-        out = self.__message[self.counter:(self.counter + self.config.board_width - 4)]
-        self.counter += 1
-        if self.counter >= len(self.__message):
-            self.counter = 0
-        return out
+        out = self.__message[self.__counter:(self.__counter + self.__config.board_width - 4)]
+        self.__counter += 1
+        if self.__counter >= len(self.__message):
+            self.__counter = 0
+
+        self.__renderer.left_message(out)
+
+
+class _AdditionalServiceFlipper:
+    __services = []
+    __counter = 0
+
+    __last_change = datetime.datetime.now()
+    __change_delta = datetime.timedelta(seconds=5)
+
+    def __init__(self, config, renderer):
+        self.__config = config
+        self.__renderer = renderer
+
+    def set_services(self, services):
+        if len(services) < 2 or self.__config.addional_services < 1:
+            self.__services = []
+        else:
+            self.__services = services[1:self.__config.addional_services]
+
+    def render(self):
+        if len(self.__services) < 1:
+            self.__renderer.message(self.__config, "*** No additional departures ***")
+            return
+
+        if (self.__last_change + self.__change_delta) < datetime.datetime.now():
+            self.__last_change = datetime.datetime.now()
+            self.__counter = self.__counter + 1
+            if self.__counter == self.__config.addional_services or (self.__counter + 1) >= len(self.__services):
+                self.__counter = 0
+
+        self.__renderer.service_row(2 + self.__counter, self.__services[1 + self.__counter])
 
 
 class Board:
-
-    lastChange = datetime.datetime.now()
-    changeDelta = datetime.timedelta(seconds=5)
-
-    dest_ticker = Ticker()
+    __config = Config()
+    __renderer = _Renderer(__config)
+    __dest_ticker = _Ticker(__config, __renderer)
+    __additional_services = _AdditionalServiceFlipper(__config, __renderer)
 
     #TODO should be passed in rather than making API calls directly
-    serviceInfo = None
+    __service_info = None
 
     # Counter & limit for additional services
-    additionalServices = 3
     serviceCounter = 0
 
     config = Config()
 
+    # TODO Handle cancelled services:
+    # "cancelReasonCode": "TG",
+    #  "cancelReasonShortText": "issue with train crew",
+    #  "cancelReasonLongText": "an issue with the train crew",
+    #  "displayAs": "CANCELLED_CALL"
+
     def render(self, services, station, platform):
+        self.__additional_services.set_services(services)
+
         self.__check_service_info(services, station)
-
-        print(self.__top())
-        print(self.__station_row(station, platform))
-        print(self.__divider())
-        print(self.__header())
-
+        self.__renderer.header(station, platform)
 
         if len(services) == 0:
-            print(self.__space_row())
-            print(self.__message("*** No departures available ***"))
-            print(self.__space_row())
-            print(self.__space_row())
+            self.__renderer.blank_row()
+            self.__renderer.message("*** No departures available ***")
+            self.__renderer.blank_row()
+            self.__renderer.blank_row()
         else:
-            print(self.__service_row(1, services[0]))
-            print(self.__calling_row())
-            print(self.__space_row())
-            print(self.__additional_service(services))
+            self.__renderer.service_row(1, services[0])
+            self.__dest_ticker.render()
+            self.__renderer.blank_row()
+            self.__additional_services.render()
 
-        print(self.__bottom_row())
+        self.__renderer.bottom_row()
 
+    def update_service_info(self, service_info):
+        self.__service_info = service_info
 
+    #TODO move API calls out of here
     def __check_service_info(self, services, station):
         if len(services) == 0:
             return
 
         service = services[0]
 
-        if self.serviceInfo is None or service.serviceUid != self.serviceInfo['serviceUid']:
-            self.serviceInfo = api.fetch_service_info(service.serviceUid, service.runDate)
+        if self.__service_info is None or service.serviceUid != self.__service_info['serviceUid']:
+            self.__service_info = api.fetch_service_info(service.serviceUid, service.runDate)
 
-            calling_points = parser.parser.calling_points(self.serviceInfo)
+            calling_points = parser.parser.calling_points(self.__service_info)
 
             stations_togo = []
             for index, point in enumerate(calling_points):
                 if point.code == station.code:
                     stations_togo = calling_points[(index + 1):]
 
-            self.dest_ticker.set_message(self.__format_calling_points(stations_togo))
+            self.__dest_ticker.set_message(self.__format_calling_points(stations_togo))
 
 
     def __format_calling_points(self, calling_points):
@@ -92,63 +192,3 @@ class Board:
                     call_str += "{} ({}), ".format(point.description, point.real_arrival)
 
         return call_str
-
-
-    def __message(self, message):
-        return "│{:^{width}}│".format(message, width=self.config.board_width - 2)
-
-
-    def __top(self):
-        return "┌{:─<{width}}┐".format('─', width=self.config.board_width - 2)
-
-    def __divider(self):
-        return "├{:─<{width}}┤".format('─', width=self.config.board_width - 2)
-
-    def __header(self):
-        return self.__board_row("", "Time", "Destination", "Expected")
-
-    def __board_row(self, order, time, destination, expected, ):
-        return "│ {:<3}{:4>}  {:{width}} {:>8} │".format(order, time, destination, expected, width=(self.config.board_width - 22))
-
-    def __station_row(self, station, platform):
-        return "│ {name:{width}} │ {platform:>12} │".format(
-            name=station.name,
-            platform="Platform {}".format(platform),
-            width=(self.config.board_width - 19)
-        )
-
-    def __space_row(self):
-        return self.__message(" ")
-
-
-    def __service_row(self, index, service):
-        expected = "On Time" if (service.expectedTime == service.departureTime) else service.expectedTime
-        return self.__board_row(index, service.departureTime, service.destination, expected)
-
-    def __bottom_row(self):
-        return "└─{:─<{leftwidth}}{:^12}{:─<{rightwidth}}─┘".format(
-            "─",
-            datetime.datetime.now().strftime("%H:%M:%S"),
-            "─",
-            leftwidth=int((self.config.board_width - 16) / 2) + (self.config.board_width % 2),
-            rightwidth=int((self.config.board_width - 16) / 2)
-        )
-
-    def __calling_row(self):
-        return "│ {:{width}} │".format(self.dest_ticker.render(), width=(self.config.board_width - 4))
-
-
-    def __additional_service(self, services):
-        if len(services) <= 1:
-            return self.__message("*** No additional departures ***")
-
-        if (self.lastChange + self.changeDelta) < datetime.datetime.now():
-            self.lastChange = datetime.datetime.now()
-            self.serviceCounter = self.serviceCounter + 1
-            if self.serviceCounter == self.additionalServices or (self.serviceCounter + 1) >= len(services):
-                self.serviceCounter = 0
-
-        return self.__service_row(2 + self.serviceCounter, services[1 + self.serviceCounter])
-
-    def __stops_list(self):
-        return self.__space_row()
