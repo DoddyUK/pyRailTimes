7734195d77b93c89090c8576f45b97ebfe6085bb
Michael Dodd <doddyuk@gmail.com>
2021-03-23 23:44:23 +0000

    Redraw test

--- test/test_board.py
+++ test/test_board.py
@@ -1,7 +1,9 @@
 import unittest
+import datetime
 from unittest.mock import patch
 from display.board import Board
 from model.station import Station
+from model.service import Service
 
 class TestBoard(unittest.TestCase):
 
@@ -57,4 +59,28 @@ class TestBoard(unittest.TestCase):
         # Verify
         renderer.show_no_departures.assert_called()
         renderer.clear_additional_service_row.assert_called()
+        renderer.commit.assert_called()
+
+
+    @patch("display.board._Renderer")
+    def test_redraw_with_services(self, mock_renderer):
+        # Given
+        renderer = mock_renderer.return_value
+        under_test = Board(0, 0)
+        station = Station("Clapham Junction", "CLJ")
+        platform = "7"
+        services = [
+            Service('a1234b', datetime.date(2021, 3, 21), '1140', 'London Waterloo', '1140', '1'),
+            Service('a5678c', datetime.date(2021, 3, 21), '1157', 'Basingstoke', '1202', '2')
+        ]
+
+        # When
+        under_test.set_station(station, platform)
+        under_test.update_services(services)
+        under_test.redraw()
+
+        # Verify
+        renderer.update_station_name.assert_called_with(station.name, platform)
+        renderer.update_primary_departure.assert_called_with(services[0])
+        renderer.update_additional_service_row.assert_called()
         renderer.commit.assert_called()
